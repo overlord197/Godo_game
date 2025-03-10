9d8278a715455788e5835795100ce76b454fba28
ZerogKill <akff472020@gmail.com>
2025-03-10 20:34:10 +0500

    Add files via upload

---
+++ player_scene.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+#var player_coins = 0
+#
+#func _ready():
+	#var ui_node = get_node("UI")
+	#if ui_node:
+		#ui_node.update_coin_label()
+#
+#func add_coins(amount):
+	#player_coins += amount
+	#var ui_node = get_node("UI")
+	#if ui_node:
+		#ui_node.add_coins(amount)
+	#print("Coins: " + str(player_coins))
